3d0821d4b0ef582fae8f854baea759ad7be13e9d
guojiyuan <gjy547773673@hotmail.com>
2021-04-01 00:38:41 +0800

    multi

--- src/app.js
+++ src/app.js
@@ -1,49 +1,23 @@
 const Koa = require('koa');
-const path = require('path');
 const serve = require('koa-static');
+const bodyParser = require('koa-bodyparser');
 var cors = require('koa2-cors');
 const routes = require('./handle/index.js')
-const fs = require('fs');
 
-// const router = new Router();
-
-const index = path.resolve(path.join('src', 'template', 'index.html'));
-
-function readFile (url) {
-  return new Promise((success) => {
-    const doc = fs.readFile(url, {encoding: 'utf8'}, (err, data) => {
-      if (err) {
-        success('')
-        return
-      }
-      success(data)
-    })
-  })
-}
-
-// module.exports = router;
 const port = 3002
-// console.log(path.resolve("./static"))
 new Koa()
   .use(cors())
   .use(serve("./static"))
   // .use(bodyParser())
-  // .use(routes.routes())
-  // .use(routes.allowedMethods())
-  // .use(async (ctx, next) => {
-  //   if (ctx.status === 404 && ctx.method === 'GET') {
-  //     ctx.redirect('/')
+  .use(routes.routes())
+  .use(routes.allowedMethods())
+  // .use(async function (ctx, next) {
+  //   if (ctx.req.method.toLowerCase() === 'get' && !path.extname(ctx.req.url)) {
+  //     const file = await readFile(index);
+  //     ctx.status = 200;
+  //     ctx.set('content-type', 'text/html')
+  //     ctx.body = file.toString();
   //   }
   //   next();
   // })
-  .use(async function (ctx, next) {
-    console.log(ctx.req.url);
-    if (ctx.req.method.toLowerCase() === 'get' && !path.extname(ctx.req.url)) {
-      const file = await readFile(index);
-      ctx.status = 200;
-      ctx.set('content-type', 'text/html')
-      ctx.body = file.toString();
-    }
-    next();
-  })
   .listen(port);
--- src/handle/index/index.js
+++ src/handle/index/index.js
@@ -4,7 +4,7 @@ const Router = require('@koa/router');
 
 const router = new Router();
 
-const index = path.resolve(path.join('src', 'template', 'index.html'));
+const basePath = path.resolve(path.join('src', 'template', '$$.html'));
 
 function readFile (url) {
   return new Promise((success) => {
@@ -17,15 +17,49 @@ function readFile (url) {
     })
   })
 }
-router.get('/', async function (ctx, next) {
-  console.log(ctx.req.url);
-  if (!path.extname(ctx.req.url)) {
-    const file = await readFile(index);
+router.get('/record-list', async function (ctx, next) {
+  const file = await readFile(basePath.replace(/[$]{2}/, 'record-list'));
+  ctx.status = 200;
+  ctx.set('content-type', 'text/html')
+  ctx.body = file.toString();
+  next();
+}).get('/record-detail', async function (ctx, next) {
+  const file = await readFile(basePath.replace(/[$]{2}/, 'record-detail'));
+  ctx.status = 200;
+  ctx.set('content-type', 'text/html')
+  ctx.body = file.toString();
+  next();
+}).get('/error', async function (ctx, next) {
+  const file = await readFile(basePath.replace(/[$]{2}/, 'error'));
+  ctx.status = 200;
+  ctx.set('content-type', 'text/html')
+  ctx.body = file.toString();
+  next();
+}).get('/agreement', async function (ctx, next) {
+  const file = await readFile(basePath.replace(/[$]{2}/, 'agreement'));
+  ctx.status = 200;
+  ctx.set('content-type', 'text/html')
+  ctx.body = file.toString();
+  next();
+}).get('/pay', async function (ctx, next) {
+  const file = await readFile(basePath.replace(/[$]{2}/, 'pay'));
+  ctx.status = 200;
+  ctx.set('content-type', 'text/html')
+  ctx.body = file.toString();
+  next();
+}).get('/video', async function (ctx, next) {
+  const file = await readFile(basePath.replace(/[$]{2}/, 'video'));
+  ctx.status = 200;
+  ctx.set('content-type', 'text/html')
+  ctx.body = file.toString();
+  next();
+}).get('/', async function (ctx, next) {
+  if (ctx.req.url === '/') {
+    const file = await readFile(basePath.replace(/[$]{2}/, 'index'));
     ctx.status = 200;
     ctx.set('content-type', 'text/html')
     ctx.body = file.toString();
   }
   next();
 });
-
 module.exports = router;
